bcc6e1781c3dfb893f1ba125d26669ce18a08ac3
Aniket Pathak <aniketpathak028@gmail.com>
2025-03-19 19:21:57 +0530

    (bug) let vercel read env var during build

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,13 +1,13 @@
 // Firebase configuration
 const firebaseConfig = {
-    apiKey: window.FIREBASE_API_KEY,
-    authDomain: window.FIREBASE_AUTH_DOMAIN,
-    databaseURL: window.FIREBASE_DATABASE_URL,
-    projectId: window.FIREBASE_PROJECT_ID,
-    storageBucket: window.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: window.FIREBASE_MESSAGING_SENDER_ID,
-    appId: window.FIREBASE_APP_ID,
-    measurementId: window.FIREBASE_MEASUREMENT_ID
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
