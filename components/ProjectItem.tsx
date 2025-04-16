602ac2e761d710a4c0f9c7672ef9278ecba41ec2
Aniket Pathak <aniketpathak028@gmail.com>
2025-04-16 19:11:14 +0000

    add link svg to project links

--- components/ProjectItem.tsx
+++ components/ProjectItem.tsx
@@ -16,6 +16,15 @@ export default function ProjectItem({
           <span className="text-sm text-indigo-400 font-bold">
             {item.title}
           </span>
+          <span>
+            <svg 
+              className="w-4 h-4 fill-current"
+              viewBox="0 0 16 16"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M15 1v6h-2V4.41L7.41 10 6 8.59 11.59 3H9V1zm-4 10a1 1 0 01-1 1H5a1 1 0 01-1-1V6a1 1 0 011-1h2V3H5a3 3 0 00-3 3v5a3 3 0 003 3h5a3 3 0 003-3V9h-2z" />
+            </svg>
+          </span>
         </a>
       </div>
 
