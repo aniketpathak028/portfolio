5fc7b16d4d34b6a10a4c962d293c8fb1375f1433
Aniket Pathak <aniketpathak028@gmail.com>
2025-06-11 20:22:39 +0000

    bug fix: resume page not redirecting back to website on back button:

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,18 +1,5 @@
-"use client";
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
+import { redirect, RedirectType } from "next/navigation";
 
 export default function Resume() {
-  const router = useRouter();
-  
-  useEffect(() => {
-    // Replace current history entry to prevent back button issues
-    window.location.replace("https://aniketpathak028.github.io/resume.pdf");
-  }, []);
-
-  return (
-    <div className="flex items-center justify-center min-h-screen">
-      <p>opening resume...</p>
-    </div>
-  );
+  redirect("https://aniketpathak028.github.io/resume.pdf", RedirectType.replace);
 }
