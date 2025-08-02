851c4828197c295c4a382c13283d8beb961bb681
Aniket <aniketpathak028@gmail.com>
2025-08-02 19:15:33 +0530

    add article borders when in mobile

--- components/ArticleListItem.tsx
+++ components/ArticleListItem.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 const ArticleListItem = ({ category, articles }: Props) => {
   return (
-    <div className="md:border md:border-gray-800 rounded-md p-4 sm:p-6 flex flex-col gap-5">
+    <div className="border border-gray-800 rounded-md p-4 sm:p-6 flex flex-col gap-5">
       <div className="text-xs px-2 py-1 text-gray-400 bg-gray-800 rounded w-fit">
         {category}
       </div>
